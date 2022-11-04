7608031b3a4a4143a7c50a8a688a04fc6fe51739
sharif.syful@gmail.com <sharif.syful@gmail.com>
2022-11-04 11:31:25 +0600

    Manual Card info added

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,55 @@ class MyApp extends StatelessWidget {
                   'MOBILE APP DEVELOPER',
                   style: TextStyle(
                       fontFamily: 'Source Sans Pro',
-                      fontSize: 15.0,
+                      fontSize: 25.0,
                       fontWeight: FontWeight.bold,
                       letterSpacing: 3.0,
                       color: Colors.white70),
                 ),
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  child: Row(children: [
+                    Icon(
+                      Icons.call,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+880 1722 686817',
+                      style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.black,
+                          fontSize: 20.0),
+                    )
+                  ]),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  child: Row(children: [
+                    Icon(
+                      Icons.mail,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'mail@sharif.live',
+                      style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.black,
+                          fontSize: 20.0),
+                    )
+                  ]),
+                )
               ],
             ),
           ),
