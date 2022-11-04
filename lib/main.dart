d6e4fcd92ea1b10ddff64ae0c4d375baed4df629
sharif.syful@gmail.com <sharif.syful@gmail.com>
2022-11-04 11:55:34 +0600

    Center Widget Removed and Main Axis Alignment Centered

--- lib/main.dart
+++ lib/main.dart
@@ -11,51 +11,48 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 50.0,
-                  // backgroundColor: Colors.red[400],
-                  backgroundImage: AssetImage('images/sharif.jpeg'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                // backgroundColor: Colors.red[400],
+                backgroundImage: AssetImage('images/sharif.jpeg'),
+              ),
+              Text(
+                'Syful Islam Sharif',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'MOBILE APP DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3.0,
+                    color: Colors.white70),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.call),
+                  title: Text('+880 1722 686817'),
                 ),
-                Text(
-                  'Syful Islam Sharif',
-                  style: TextStyle(
-                      fontFamily: 'Pacifico',
-                      fontSize: 30.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.mail),
+                  title: Text('mail@sharif.live'),
                 ),
-                Text(
-                  'MOBILE APP DEVELOPER',
-                  style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 3.0,
-                      color: Colors.white70),
-                ),
-                Card(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  color: Colors.white,
-                  child: ListTile(
-                    leading: Icon(Icons.call),
-                    title: Text('+880 1722 686817'),
-                  ),
-                ),
-                Card(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  color: Colors.white,
-                  child: ListTile(
-                    leading: Icon(Icons.mail),
-                    title: Text('mail@sharif.live'),
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
